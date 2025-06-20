a72aba963d192ebe26fdf2510e1378f74b0a83a8
Henry <cootshk@duck.com>
2025-06-19 19:13:32 -0500

    Outfit shenanigans

--- src/main/java/studio/dreamys/prometheus/mixin/com/lunarclient/websocket/cosmetic/v2/MixinCosmeticService$Stub.java
+++ src/main/java/studio/dreamys/prometheus/mixin/com/lunarclient/websocket/cosmetic/v2/MixinCosmeticService$Stub.java
@@ -4,6 +4,7 @@ package studio.dreamys.prometheus.mixin.com.lunarclient.websocket.cosmetic.v2;
 import com.lunarclient.websocket.cosmetic.v2.LoginResponse;
 import com.lunarclient.websocket.cosmetic.v2.CosmeticService;
 import com.lunarclient.websocket.cosmetic.v2.EquippedCosmetic;
+import com.lunarclient.websocket.cosmetic.v2.Outfit;
 //import com.lunarclient.websocket.cosmetic.v2.CustomizableCosmeticSettings;
 import com.lunarclient.websocket.cosmetic.v2.LoginRequest;
 import com.lunarclient.websocket.cosmetic.v2.UpdateOutfitRequest;
@@ -24,14 +25,20 @@ import java.util.List;
 public class MixinCosmeticService$Stub {
     @Overwrite
     public void login(RpcController rpcController, LoginRequest loginRequest, RpcCallback<LoginResponse> rpcCallback) {
+        System.out.println("[Prometheus] Intercepted login request to load equipped cosmetics...");
         //build our fake response
         LoginResponse loginResponse = LoginResponse.newBuilder().setHasAllCosmeticsFlag(true).build();
         EquippedCosmetic settings = EquippedCosmetic.newBuilder().setClothCloak(true).build();
 
         //set our equipped cosmetics from file
         List<EquippedCosmetic> equippedCosmetics = FileUtils.read();
-//        ReflectionUtils.set(settings, "equippedCosmetics_", equippedCosmetics);
-//        ReflectionUtils.set(loginResponse, "settings_", settings);
+        Outfit equippedOutfit = Outfit.newBuilder()
+                .addAllCosmetics(equippedCosmetics)
+                .build();
+        List<Outfit> outfits = loginResponse.getOutfitsList();
+        outfits.add(equippedOutfit);
+        ReflectionUtils.set(loginResponse, "outfits_", outfits);
+        System.out.println("[Prometheus] Loaded " + equippedCosmetics.size() + " equipped cosmetics from file.");
 
         //let lunar handle the rest
         rpcCallback.run(loginResponse);
@@ -39,13 +46,15 @@ public class MixinCosmeticService$Stub {
 
     @Inject(method = "updateOutfit", at = @At("TAIL"))
     public void updateOutfit(RpcController rpcController, UpdateOutfitRequest updateOutfitRequest, RpcCallback<UpdateOutfitRequest> rpcCallback, CallbackInfo ci) {
+        System.out.println("[Prometheus] Updating equipped cosmetics...");
         //get equipped cosmetics from the update request (triggered every wardrobe change)
-        List<EquippedCosmetic> equippedCosmetics = updateOutfitRequest.getSettings().getEquippedCosmeticsList();
+        List<EquippedCosmetic> equippedCosmetics = updateOutfitRequest.getOutfit().getCosmeticsList();
 
         //remove metadata fields (ex: pets) which cause adapter issues for gson
-        equippedCosmetics.forEach(equippedCosmetic -> ReflectionUtils.set(equippedCosmetic.getMetadata(), "fields_", null));
+        equippedCosmetics.forEach(equippedCosmetic -> ReflectionUtils.set(equippedCosmetic.getGeckolibMetadata(), "fields_", null));
 
         //save
         FileUtils.write(equippedCosmetics);
+        System.out.println("[Prometheus] Saved " + equippedCosmetics.size() + " equipped cosmetics to file.");
     }
 }
