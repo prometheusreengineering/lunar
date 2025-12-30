60bb48c276e8eab18a5a2f9c6e24dc2988c5c526
Serhiy <owner@dreamys.studio>
2025-12-29 22:16:08 -0500

    feat: impl patch correctly (revert + fixes)

--- src/main/java/studio/dreamys/prometheus/mixin/com/lunarclient/websocket/cosmetic/v2/MixinCosmeticService$Stub.java
+++ src/main/java/studio/dreamys/prometheus/mixin/com/lunarclient/websocket/cosmetic/v2/MixinCosmeticService$Stub.java
@@ -2,18 +2,37 @@ package studio.dreamys.prometheus.mixin.com.lunarclient.websocket.cosmetic.v2;
 
 import com.google.protobuf.RpcCallback;
 import com.google.protobuf.RpcController;
-import com.lunarclient.websocket.cosmetic.v2.CosmeticService;
-import com.lunarclient.websocket.cosmetic.v2.UpdateOutfitRequest;
+import com.lunarclient.websocket.cosmetic.v2.*;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Overwrite;
 import studio.dreamys.prometheus.util.FileUtils;
 
 @Mixin(value = CosmeticService.Stub.class, remap = false)
 public class MixinCosmeticService$Stub {
-    @Inject(method = "updateOutfit", at = @At("TAIL"))
-    public void updateOutfit(RpcController rpcController, UpdateOutfitRequest updateOutfitRequest, RpcCallback<UpdateOutfitRequest> rpcCallback, CallbackInfo ci) {
-        FileUtils.write(updateOutfitRequest.getOutfit());
+    @Overwrite
+    public void login(RpcController rpcController, LoginRequest loginRequest, RpcCallback<LoginResponse> rpcCallback) {
+        System.out.println("[Prometheus] Patched cosmetic service login request.");
+
+        //load saved outfit from file
+        Outfit outfit = FileUtils.readOutfit();
+
+        //create our own response
+        LoginResponse response = LoginResponse.newBuilder()
+                .setHasAllCosmeticsFlag(true)
+                .setArtistTools(true)
+                .addOutfits(outfit)
+                .setOutfitTree(OutfitTree.newBuilder().setDefaultOutfitId(outfit.getId()).build())
+                .build();
+
+        //let lunar handle the rest
+        rpcCallback.run(response);
+    }
+
+    @Overwrite
+    public void updateOutfit(RpcController rpcController, UpdateOutfitRequest updateOutfitRequest, RpcCallback<UpdateOutfitRequest> rpcCallback) {
+        System.out.println("[Prometheus] Patched cosmetic service update outfit request.");
+
+        //save outfit to file
+        FileUtils.writeOutfit(updateOutfitRequest.getOutfit());
     }
 }
